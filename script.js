b5637f7b86a1f44d3f9bef811e0b195213f26c99
Sanjana Ganesh Nayak <72573321+Sanj24Dev@users.noreply.github.com>
2021-05-31 20:49:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+function showBlack()
+{
+    document.getElementById('diffcolors').src="https://imgur.com/hg9SG3g.png";
+}
+
+function showc2()
+{
+    document.getElementById('diffcolors').src="https://imgur.com/TM7PeXd.png";
+}
+
+function showc3()
+{
+    document.getElementById('diffcolors').src="https://imgur.com/Rw5d77x.png";
+}
+
+function showc4()
+{
+    document.getElementById('diffcolors').src="https://imgur.com/Qzor5fb.png";
+}
+
+function showc5()
+{
+    document.getElementById('diffcolors').src="https://imgur.com/mUnM3IH.png";
+}
+
+function showc6()
+{
+    document.getElementById('diffcolors').src="https://imgur.com/x7YSWB1.png";
+}
+
+function showc7()
+{
+    document.getElementById('diffcolors').src="https://imgur.com/UrpJE7Z.png";
+}
+
+function showc8()
+{
+    document.getElementById('diffcolors').src="https://imgur.com/NhNLYpm.png";
+}
+
